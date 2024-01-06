c355c909a9145ab3a1b806491ff82c2a26ac4695
Pushpraj Singh <pushprajsingh407@gmail.com>
2024-01-06 23:25:50 +0530

    created register component

--- frontend/todo/src/app/app.routes.ts
+++ frontend/todo/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './user/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'user/register', component: RegisterComponent 
+    }
+];
