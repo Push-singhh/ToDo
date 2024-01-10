8dee55c6aca913e13b02b48cfe4e725f67ab8aff
Pushpraj Singh <pushprajsingh407@gmail.com>
2024-01-10 23:38:53 +0530

    create category dialog

---
+++ frontend/todo/src/app/dialogs/create-category-dialog/create-category-dialog.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { CrudService } from '../../services/crud.service';
+import { MatDialogActions, MatDialogClose, MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-create-category-dialog',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatDialogActions,
+    MatDialogClose,
+    ReactiveFormsModule
+  ],
+  templateUrl: './create-category-dialog.component.html',
+  styleUrl: './create-category-dialog.component.css'
+})
+export class CreateCategoryDialogComponent {
+  categoryForm:any
+
+  constructor(
+    private fb: FormBuilder,
+    private crudService: CrudService
+  ) {}
+
+  ngOnInit() {
+    this.categoryForm = this.fb.group({
+      title : ['', Validators.required]
+    })
+  }
+
+  submit() {
+    console.log(this.categoryForm)
+    this.crudService.postData("categories/", {
+      title: this.categoryForm.get("title").value
+    }).subscribe((data: any) => {
+
+    })
+  }
+}
--- frontend/todo/src/app/todo/categories/categories.component.ts
+++ frontend/todo/src/app/todo/categories/categories.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
 import { CrudService } from '../../services/crud.service';
 import { CdkDragDrop, moveItemInArray, CdkDropList, CdkDrag} from '@angular/cdk/drag-drop'
+import { MatDialog } from '@angular/material/dialog'
+import { CreateCategoryDialogComponent } from '../../dialogs/create-category-dialog/create-category-dialog.component';
 
 @Component({
   selector: 'app-categories',
@@ -21,7 +23,8 @@ export class CategoriesComponent {
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
-    private crudService: CrudService
+    private crudService: CrudService,
+    private dialog: MatDialog
   ) {
     this.user['email'] = localStorage.getItem("email")
     this.user['name'] = localStorage.getItem("name")
@@ -35,6 +38,17 @@ export class CategoriesComponent {
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.categories, event.previousIndex, event.currentIndex);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CreateCategoryDialogComponent, {
+      width: '300px',
+      
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.get_categories()
+    });
   }
 
   submit() {
