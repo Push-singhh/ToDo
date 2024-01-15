954eb08b9416526c8e9da1e1b2fdae5064a306df
Pushpraj Singh <pushprajsingh407@gmail.com>
2024-01-15 23:12:15 +0530

    CategoryUpdateAPIView added

--- backend/category/views.py
+++ backend/category/views.py
@@ -1,8 +1,11 @@
-from rest_framework import generics, authentication, permissions
+from rest_framework import generics, authentication, permissions, status
+from rest_framework.response import Response
 
 from .models import Category
 from .serializers import CategorySerializer
 from api.mixins import UserQuerySetMixin
+from task.views import move_item
+from rest_framework.exceptions import ValidationError
 
 
 class CategoryListCreateAPIView(UserQuerySetMixin,
@@ -10,15 +13,31 @@ class CategoryListCreateAPIView(UserQuerySetMixin,
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def create(self, request, *args, **kwargs):
+
+        last_record = self.get_queryset().last()
+        if last_record:
+            last_position = last_record.position
+        else:
+            last_position = 0
+        data = request.data
+        data.update({"position": last_position + 1})
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    # def get_queryset(self, *args, **kwargs):
-    #     qs = super().get_queryset()
-    #     user = self.request.user
-    #     if not user.is_authenticated:
-    #         return Category.objects.none()
-    #     return qs.filter(user=self.request.user)
+    def get_queryset(self):
+        """
+        Return Categories of currently authenticated user
+        """
+
+        user = self.request.user
+        return Category.objects.filter(user=user).order_by('position')
 
 
 category_list_create_view = CategoryListCreateAPIView.as_view()
@@ -35,7 +54,23 @@ category_detail_view = CategoryDetailAPIView.as_view()
 class CategoryUpdateAPIView(generics.UpdateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    lookup_field = 'pk'
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        if (request.data.get('position') and instance.position and
+                instance.position != request.data.get('position')):
+            move_item(instance.position, request.data.get('position'), Category)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
 
 category_update_view = CategoryUpdateAPIView.as_view()
