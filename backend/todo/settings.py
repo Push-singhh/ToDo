87ab1872a50d94b19f209ca9a25f10b22f43770f
Pushpraj Singh <pushprajsingh407@gmail.com>
2024-01-17 10:20:15 +0530

    cors update

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -63,7 +63,8 @@ MIDDLEWARE = [
 
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200'
+    'http://localhost:4200',
+    'http://13.49.91.201'
 ]
 
 ROOT_URLCONF = 'todo.urls'
