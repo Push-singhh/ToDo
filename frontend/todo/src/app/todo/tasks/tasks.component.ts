677503ba155f42c4a128d561240bc7017599e0db
Pushpraj Singh <pushprajsingh407@gmail.com>
2024-01-26 23:25:38 +0530

    checkbox to mark task as completed, and separate lists of completed and uncompleted tasks

--- frontend/todo/src/app/todo/tasks/tasks.component.ts
+++ frontend/todo/src/app/todo/tasks/tasks.component.ts
@@ -5,20 +5,31 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateTaskDialogComponent } from '../../dialogs/create-task-dialog/create-task-dialog.component';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'
+import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [
-    CdkDropList, CdkDrag, MatButtonModule
+    CdkDropList, CdkDrag, MatButtonModule, MatCardModule, MatButtonModule, MatIconModule, 
+    MatCheckboxModule, MatRadioModule, FormsModule
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   tasks:any = []
-  category_id!:number
+  completedTasks: any = []
   selectedTaskId!:number
+  category_id!:number
+  category_details: any
+
+  completed = false
 
   constructor(
     private crudService: CrudService,
@@ -30,20 +41,53 @@ export class TasksComponent {
       this.category_id = data.id
       this.selectedTaskId = data.task_id
       if(this.category_id) {
-        this.getTasks(this.category_id)
+        this.getUncompletedTasks()
+        this.getCompletedTasks()
       }
+      this.getCategoryDetails()
     })
   }
 
-  getTasks(category_id: number) {
-    this.crudService.getAllData(`tasks/?category_id=${category_id}`).subscribe((data:any) => {
+  getUncompletedTasks() {
+    this.crudService.getAllData(`tasks/?category_id=${this.category_id}&completed=${false}`).subscribe((data:any) => {
       this.tasks = data
+    })
+  }
+
+  getCompletedTasks() {
+    this.crudService.getAllData(`tasks/?category_id=${this.category_id}&completed=${true}`).subscribe((data:any) => {
+      this.completedTasks = data
     })
   }
 
   taskDetails(task_id:number) {
     this.selectedTaskId = task_id
     this.router.navigate(['/todo-board/category', this.category_id, 'task', task_id])
+  }
+
+  
+  getCategoryDetails() {
+    this.crudService.getAllData(`categories/${this.category_id}`).subscribe((data:any) => {
+      this.category_details = data
+    })
+  }
+
+  updateTaskCompletion(task_id: number, completed_at:any, event:any) {
+    console.log(event)
+    if (completed_at) {
+      completed_at = new Date()
+    }
+    else {
+      completed_at = null
+    }
+    this.crudService.updateData(`tasks/${task_id}/update`, {
+      completed_at: completed_at
+    }).subscribe((response:any) => {
+        this.getUncompletedTasks()
+        this.getCompletedTasks()
+    })
+    event.stopPropagation();
+
   }
 
   drop(event: CdkDragDrop<string[]>) {
@@ -66,7 +110,7 @@ export class TasksComponent {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      this.getTasks(this.category_id)
+      this.getUncompletedTasks()
     });
   }
 
